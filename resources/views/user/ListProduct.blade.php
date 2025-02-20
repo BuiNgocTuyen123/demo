932700e1c2c7bab2bcaf79b896057877f1e77af2
Bui Ngoc Tuyen <bntuyena2.2019c3locthanh@gmail.com>
2025-02-20 09:07:26 +0700

    Cập nhật ListProduct.blade.php - thêm 2 dòng code

--- resources/views/user/ListProduct.blade.php
+++ resources/views/user/ListProduct.blade.php
@@ -4,6 +4,8 @@
         <td class="lu">a</td>
         <td class="lu">b</td>
         <td class="lu">c</td>
+        <td class="lu">d</td>
+        <td class="lu">e</td>
     </tr>
 </table>
 </div>
